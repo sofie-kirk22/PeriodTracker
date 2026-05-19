ca067f6b263f0c00f5d26301532614e98e9788e9
sofie-kirk22 <133001261+sofie-kirk22@users.noreply.github.com>
2026-05-19 12:54:14 +0200

    added api endpoints and random cycle generator for testing purposes.

--- PeriodTracker/Program.cs
+++ PeriodTracker/Program.cs
@@ -8,12 +8,14 @@ var tracker = new CycleTracker();
 
 var randomCycles = CycleGenerator.GenerateCycles(6, new DateTime(2024, 5, 24));
 
+tracker.Cycles = storage.LoadCycles();
+
 foreach (var cycle in randomCycles)
 {
     tracker.AddCycle(cycle);
 }
 
-tracker.Cycles = storage.LoadCycles();
+/*
 tracker.AddCycle(new Cycle
 {
     MenstrualPhase = new MenstrualPhase
@@ -42,125 +44,65 @@ tracker.AddCycle(new Cycle
         MoodLevel = 2
     }
 });
-
-tracker.AddCycle(new Cycle
-{
-    MenstrualPhase = new MenstrualPhase
-    {
-        StartDate = new DateTime(2024, 1, 29),
-        EndDate = new DateTime(2024, 2, 2),
-        FlowIntensity = 3
-    },
-    FollicularPhase = new FollicularPhase
-    {
-        StartDate = new DateTime(2024, 2, 3),
-        EndDate = new DateTime(2024, 2, 10),
-        EnergyLevel = 4
-    },
-    Ovulation = new Ovulation
-    {
-        FertileWindowStart = new DateTime(2024, 2, 11),
-        FertileWindowEnd = new DateTime(2024, 2, 13),
-        OvulationDate = new DateTime(2024, 2, 12),
-        FertilityLevel = 4
-    },
-    LutealPhase = new LutealPhase
-    {
-        StartDate = new DateTime(2024, 2, 14),
-        EndDate = new DateTime(2024, 2, 25),
-        MoodLevel = 3
-    }
-});
-
-tracker.AddCycle(new Cycle
-{
-    MenstrualPhase = new MenstrualPhase
-    {
-        StartDate = new DateTime(2024, 2, 26),
-        EndDate = new DateTime(2024, 3, 2),
-        FlowIntensity = 2
-    },
-    FollicularPhase = new FollicularPhase
-    {
-        StartDate = new DateTime(2024, 3, 3),
-        EndDate = new DateTime(2024, 3, 12),
-        EnergyLevel = 3
-    },
-    Ovulation = new Ovulation
-    {
-        FertileWindowStart = new DateTime(2024, 3, 13),
-        FertileWindowEnd = new DateTime(2024, 3, 16),
-        OvulationDate = new DateTime(2024, 3, 14),
-        FertilityLevel = 3
-    },
-    LutealPhase = new LutealPhase
-    {
-        StartDate = new DateTime(2024, 3, 17),
-        EndDate = new DateTime(2024, 3, 30),
-        MoodLevel = 1
-    }
-});
-
-tracker.AddCycle(new Cycle
-{
-    MenstrualPhase = new MenstrualPhase
-    {
-        StartDate = new DateTime(2024, 3, 31),
-        EndDate = new DateTime(2024, 4, 4),
-        FlowIntensity = 2
-    },
-    FollicularPhase = new FollicularPhase
-    {
-        StartDate = new DateTime(2024, 4, 5),
-        EndDate = new DateTime(2024, 4, 9),
-        EnergyLevel = 2
-    },
-    Ovulation = new Ovulation
-    {
-        FertileWindowStart = new DateTime(2024, 4, 10),
-        FertileWindowEnd = new DateTime(2024, 4, 12),
-        OvulationDate = new DateTime(2024, 4, 11),
-        FertilityLevel = 3
-    },
-    LutealPhase = new LutealPhase
-    {
-        StartDate = new DateTime(2024, 4, 13),
-        EndDate = new DateTime(2024, 4, 24),
-        MoodLevel = 2
-    }
-});
-
-tracker.AddCycle(new Cycle
-{
-    MenstrualPhase = new MenstrualPhase
-    {
-        StartDate = new DateTime(2024, 4, 25),
-        EndDate = new DateTime(2024, 4, 30),
-        FlowIntensity = 4
-    },
-    FollicularPhase = new FollicularPhase
-    {
-        StartDate = new DateTime(2024, 5, 1),
-        EndDate = new DateTime(2024, 5, 8),
-        EnergyLevel = 2
-    },
-    Ovulation = new Ovulation
-    {
-        FertileWindowStart = new DateTime(2024, 5, 9),
-        FertileWindowEnd = new DateTime(2024, 5, 11),
-        OvulationDate = new DateTime(2024, 5, 10),
-        FertilityLevel = 2
-    },
-    LutealPhase = new LutealPhase
-    {
-        StartDate = new DateTime(2024, 5, 12),
-        EndDate = new DateTime(2024, 5, 23),
-        MoodLevel = 1
-    }
-});
+*/
 
 storage.SaveCycles(tracker.Cycles);
 
 app.MapGet("/", () => "Small and simple period tracker API");
+
+app.MapGet("/current-phase", () =>
+{
+    var today = DateTime.Today;
+    return tracker.GetCurrentPhase(today);
+});
+
+app.MapGet("/average-cycle-length", () =>
+{
+    return tracker.GetAverageCycleLength();
+});
+
+app.MapGet("/days-to-ovulation", () =>
+{
+    return tracker.GetDaysToOvulation();
+});
+
+app.MapGet("/cycles", () =>
+{
+    return tracker.Cycles.Select(c => new
+    {
+        CycleStart = c.CycleStartDate.ToString("yyyy-MM-dd"),
+        CycleEnd = c.CycleEndDate.ToString("yyyy-MM-dd"),
+        MenstrualPhase = new
+        {
+            Start = c.MenstrualPhase.StartDate.ToString("yyyy-MM-dd"),
+            End = c.MenstrualPhase.EndDate.ToString("yyyy-MM-dd"),
+            FlowIntensity = c.MenstrualPhase.FlowIntensity
+        },
+        FollicularPhase = new
+        {
+            Start = c.FollicularPhase.StartDate.ToString("yyyy-MM-dd"),
+            End = c.FollicularPhase.EndDate.ToString("yyyy-MM-dd"),
+            EnergyLevel = c.FollicularPhase.EnergyLevel
+        },
+        Ovulation = new
+        {
+            FertileWindowStart = c.Ovulation.FertileWindowStart.ToString("yyyy-MM-dd"),
+            FertileWindowEnd = c.Ovulation.FertileWindowEnd.ToString("yyyy-MM-dd"),
+            OvulationDate = c.Ovulation.OvulationDate.ToString("yyyy-MM-dd"),
+            FertilityLevel = c.Ovulation.FertilityLevel
+        },
+        LutealPhase = new
+        {
+            Start = c.LutealPhase.StartDate.ToString("yyyy-MM-dd"),
+            End = c.LutealPhase.EndDate.ToString("yyyy-MM-dd"),
+            MoodLevel = c.LutealPhase.MoodLevel
+        }
+    });
+});
+
+app.MapGet("/list-all-cycles", () =>
+{
+    return tracker.Cycles;
+});
 
 app.Run();
